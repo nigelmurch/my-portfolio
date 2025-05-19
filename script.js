baf8e556c268bbbe22be6d7c7a8967ef224b9cfc
Nigel Murch <a642171@imac.lan>
2025-05-19 13:26:07 +1200

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+
+// This is the entry point for your JavaScript code
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+    
+    // Your code here
+});
