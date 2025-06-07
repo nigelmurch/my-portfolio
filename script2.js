5bde8fca3d49865d9001cafc0d275be419a2cdda
nigel-murch <nigelmurch@xtra.co.nz>
2025-06-07 12:14:20 +1200

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,1 +1,17 @@
+const modal = document.getElementById('modal-overlay');
+const openModalBtn = document.getElementById('openModal');
+const closeModalBtn = document.getElementById('closeModal');
 
+openModalBtn.addEventListener('click', () => {
+  modal.style.display = 'flex';
+});
+
+closeModalBtn.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
+
+window.addEventListener('click', (e) => {
+  if (e.target === modal) {
+    modal.style.display = 'none';
+  }
+});
