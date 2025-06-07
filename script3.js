483ebbd71580e12e6c12255f4f327fb38c991fe8
nigel-murch <nigelmurch@xtra.co.nz>
2025-06-07 15:31:11 +1200

    Update script3.js

--- script3.js
+++ script3.js
@@ -2,7 +2,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const nextBtn = document.getElementById("nextBtn");
 
-  nextBtn.addEventListener("click", (button) => {
+  nextBtn.addEventListener("click", (next) => {
     // Redirect to another page or show more content
     window.location.href = "f.b.p.html"; // Change this to your actual next page
   });
