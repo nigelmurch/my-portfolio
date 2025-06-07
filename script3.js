44ded605e899d93ca3bdf437e219c9654ff83e21
nigel-murch <nigelmurch@xtra.co.nz>
2025-06-07 15:27:35 +1200

    Update script3.js

--- script3.js
+++ script3.js
@@ -1,1 +1,9 @@
 
+document.addEventListener("DOMContentLoaded", () => {
+  const nextBtn = document.getElementById("nextBtn");
+
+  nextBtn.addEventListener("click", () => {
+    // Redirect to another page or show more content
+    window.location.href = "about.html"; // Change this to your actual next page
+  });
+});
