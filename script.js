801b886428e63947d8764f41874cb4ab0f0de65b
nigel-murch <nigelmurch@xtra.co.nz>
2025-06-15 16:04:07 +1200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ window.onload = function () {
   const bootText2 = document.querySelector('.boot-text2');
   const anyKey = document.querySelector('.any-key');
 
-  // Simulate typing effect
   const originalText = bootText2.innerHTML;
   bootText2.innerHTML = '';
   let index = 0;
@@ -12,9 +11,14 @@ window.onload = function () {
     if (index < originalText.length) {
       bootText2.innerHTML += originalText.charAt(index);
       index++;
-      setTimeout(typeText, 40); // typing speed
+      setTimeout(typeText, 40);
     } else {
       anyKey.style.display = 'block';
+
+      // Redirect on any key press
+      document.addEventListener('keydown', () => {
+        window.location.href = 'about.html';
+      });
     }
   }
 
