ecf30331f377b6b70d11df7059f0f43a78c990dc
nigel-murch <nigelmurch@xtra.co.nz>
2025-07-02 14:36:26 +1200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-function goToAboutPage() {
-  window.location.href = "about.html"; // Navigate to About page
-}
 
-// Key press event
-document.addEventListener('keydown', goToAboutPage);
-
-// Mouse click on blinking text
-document.querySelector('.blink').addEventListener('click', goToAboutPage);
